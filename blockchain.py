c813f09b2b4a7c555a2176c0656af70a0402b24c
Pathum312 <pathum.2019557@iit.ac.lk>
2024-12-14 18:36:26 +0530

    formatted

--- blockchain.py
+++ blockchain.py
@@ -1,52 +1,57 @@
-import json
 import hashlib
+import json
 from time import time
 
-TransactionDict = dict[str, str | int] # Dict representation of a transaction
-BlockDict = dict[str, str | int | float | list[TransactionDict]] # Dict representation of a block
+TransactionDict = dict[str, str | int]  # Dict representation of a transaction
+BlockDict = dict[
+    str, str | int | float | list[TransactionDict]
+]  # Dict representation of a block
+
 
 class Transaction:
     """
     Transaction class
-    
+
     Attributes:
         sender (str): Address of the sender
         recipient (str): Address of the recipient
         amount (int): Transaction amount
     """
+
     def __init__(self, sender: str, recipient: str, amount: int) -> None:
         """
         Initializes a new transaction
-        
+
         Parameters:
             sender (str): Address of the sender
             recipient (str): Address of the recipient
             amount (int): Transaction amount
-        
+
         Returns:
             None
         """
-        self.sender: str = sender # Address of the sender
-        self.recipient: str = recipient # Address of the recipient
-        self.amount: int = amount # Transaction amount
-    
+        self.sender: str = sender  # Address of the sender
+        self.recipient: str = recipient  # Address of the recipient
+        self.amount: int = amount  # Transaction amount
+
     def to_dict(self) -> TransactionDict:
         """
         Converts the transaction to a dictionary
-        
+
         Returns:
             dict: Dictionary representation of the transaction
         """
         return {
-            'sender': self.sender,
-            'recipient': self.recipient,
-            'amount': self.amount
+            "sender": self.sender,
+            "recipient": self.recipient,
+            "amount": self.amount,
         }
+
 
 class Block:
     """
     Block class
-    
+
     Attributes:
         index (int): Index of the block
         timestamp (int): Timestamp
@@ -54,71 +59,78 @@ class Block:
         proof (int): Proof of work
         previous_hash (str): Hash of the previous block
     """
+
     def __init__(
-        self, 
-        index: int, 
-        timestamp: float, 
-        transactions: list[Transaction], 
-        proof: int, 
-        previous_hash: str
-        ) -> None:
+        self,
+        index: int,
+        timestamp: float,
+        transactions: list[Transaction],
+        proof: int,
+        previous_hash: str,
+    ) -> None:
         """
         Initializes a new block
-        
+
         Parameters:
             index (int): Index of the block
             timestamp (int): Timestamp
             transactions (list[Transaction]): List of transactions in the block
             proof (int): Proof of work
             previous_hash (str): Hash of the previous block
-        
+
         Returns:
             None
         """
-        self.index: int = index # Index of the block
-        self.timestamp: float = timestamp # Timestamp
-        self.transactions: list[Transaction] = transactions # List of transactions in the block
-        self.proof: int = proof # Proof of work
-        self.previous_hash: str = previous_hash # Hash of the previous block
-    
+        self.index: int = index  # Index of the block
+        self.timestamp: float = timestamp  # Timestamp
+        self.transactions: list[Transaction] = (
+            transactions  # List of transactions in the block
+        )
+        self.proof: int = proof  # Proof of work
+        self.previous_hash: str = previous_hash  # Hash of the previous block
+
     def to_dict(self) -> BlockDict:
         """
         Converts the block to a dictionary
-        
+
         Returns:
             dict: Dictionary representation of the block
         """
         return {
-            'index': self.index,
-            'timestamp': self.timestamp,
-            'transactions': [transaction.to_dict() for transaction in self.transactions],
-            'proof': self.proof,
-            'previous_hash': self.previous_hash
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "transactions": [
+                transaction.to_dict() for transaction in self.transactions
+            ],
+            "proof": self.proof,
+            "previous_hash": self.previous_hash,
         }
+
 
 class Blockchain:
     """
     Blockchain class
-    
+
     Attributes:
         chain (list[Block]): List of blocks
         current_transactions (list[Transaction]): List of transactions
     """
+
     def __init__(self) -> None:
-        self.chain: list[Block] = [] # List of blocks
-        self.current_transactions: list[Transaction] = [] # List of transactions
-        
+        self.chain: list[Block] = []  # List of blocks
+        self.current_transactions: list[Transaction] = []  # List of transactions
+
         # Create the genesis block
-        self.new_block(proof=100, previous_hash='1')
-    
+        self.new_block(proof=100, previous_hash="1")
+
     def new_block(self, proof: int, previous_hash: str | None = None) -> Block:
         """
         Creates a new block in the blockchain
-        
+
         Parameters:
             proof (int): Proof of work
             previous_hash (str | None): Hash of the previous block
-        
+
         Returns:
             Block: The new block
         """
@@ -128,100 +140,99 @@ class Blockchain:
             timestamp=time(),
             transactions=self.current_transactions,
             proof=proof,
-            previous_hash=previous_hash or self.hash(block=self.chain[-1])
+            previous_hash=previous_hash or self.hash(block=self.chain[-1]),
         )
-        
+
         # Reset the current list of transactions
         self.current_transactions = []
-        
+
         # Add the block to the chain
         self.chain.append(block)
-        
+
         # Return the new block
         return block
-    
+
     def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
         """
         Creates a new transaction to go into the next mined Block
-        
+
         Parameters:
             sender (str): Address of the sender
             recipient (str): Address of the recipient
             amount (int): Transaction amount
-        
+
         Returns:
             int: The index of the Block that will hold this transaction
         """
         # Create a new transaction
         transaction = Transaction(sender=sender, recipient=recipient, amount=amount)
-        
+
         # Add the new transaction to the list of transactions
         self.current_transactions.append(transaction)
-        print(self.current_transactions)
-        
+
         # Returns the index of the block that will hold this transaction
         return self.last_block.index + 1
-    
+
     @staticmethod
     def hash(block: Block) -> str:
         """
         Creates a SHA-256 hash of a Block
-        
+
         Parameters:
             block (Block): Block to hash
-        
+
         Returns:
             str: Hash of the block
         """
         # Make sure the dictionary is ordered, or we'll have inconsistent hashes
         block_string: bytes = json.dumps(obj=block, sort_keys=True).encode()
         return hashlib.sha256(string=block_string).hexdigest()
-    
+
     def proof_of_work(self, last_proof: int) -> int:
         """
         Simple Proof of Work Algorithm:
         - Find a number p' such that hash(p, p') contains leading 4 zeroes.
         - p is the previous proof, p' is the new proof.
-        
+
         Parameters:
             last_proof (int): Previous Proof (p)
-        
+
         Returns:
             int: New Proof (p')
         """
         proof: int = 0
-        
+
         while self.validate_proof(last_proof=last_proof, proof=proof) is False:
             proof += 1
-        
+
         return proof
-        
+
     @staticmethod
     def validate_proof(last_proof: int, proof: int) -> bool:
         """
         Validates a Proof to see if the hash has 4 leading zeroes
-        
+
         Parameters:
             last_proof (int): Previous Proof
             proof (int): Current Proof
-        
+
         Returns:
             bool: True if correct, False if not
         """
         # Encode the last proof and the current proof
-        guess: bytes = f'{last_proof}{proof}'.encode()
-        
+        guess: bytes = f"{last_proof}{proof}".encode()
+
         # Hash the guess with SHA-256
         guess_hash: str = hashlib.sha256(string=guess).hexdigest()
-        
+
         # Check if the guess hash has 4 leading zeroes
-        return guess_hash[:4] == '0000'
-    
+        return guess_hash[:4] == "0000"
+
     @property
     def last_block(self) -> Block:
         """
         Returns the last Block in the chain
-        
+
         Returns:
             Block: The last Block in the chain
         """
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from typing import Literal
+
+from blockchain import Blockchain, BlockDict
+from flask import Flask, jsonify, request
+from flask.wrappers import Response
+
 # from textwrap import dedent
 from flask_cors import CORS
-from flask.wrappers import Response
-from flask import Flask, jsonify, request
-from blockchain import Blockchain, BlockDict
 
 # Instantiate the blockchain
 blockchain = Blockchain()
@@ -14,41 +16,54 @@ app = Flask(import_name=__name__)
 # Enable CORS
 CORS(app=app)
 
-@app.route(rule='/mine', methods=['GET'])
+
+@app.route(rule="/mine", methods=["GET"])
 def mine() -> tuple[Response, Literal[200]]:
-    response: dict[str, str] = {'message': 'Mine a new block.'}
-    
+    response: dict[str, str] = {"message": "Mine a new block."}
+
     return jsonify(response), 200
 
-@app.route(rule='/transactions/new', methods=['POST'])
+
+@app.route(rule="/transactions/new", methods=["POST"])
 def new_transaction() -> tuple[Response, Literal[201] | Literal[400]]:
     # Get the request payload
     payload: dict[str, str | int] = request.get_json()
-    
+
     # Check if all the required fields are present
-    required: list[str] = ['sender', 'recipient', 'amount']
+    required: list[str] = ["sender", "recipient", "amount"]
     for field in required:
         if not payload.get(field):
-            return jsonify({'message': f'{f'{field}s address' if field != 'amount' else field} is required.'}), 400
-    
+            return (
+                jsonify(
+                    {
+                        "message": f"{f'{field}s address' if field != 'amount' else field} is required."
+                    }
+                ),
+                400,
+            )
+
     # Create a new transaction
     index: int = blockchain.new_transaction(
-        sender=payload.get('sender'), # type: ignore
-        recipient=payload.get('recipient'), # type: ignore
-        amount=payload.get('amount') # type: ignore
+        sender=payload.get("sender"),  # type: ignore
+        recipient=payload.get("recipient"),  # type: ignore
+        amount=payload.get("amount"),  # type: ignore
     )
-    
-    response: dict[str, str] = {'message': f'Transaction will be added to Block {index}.'}
+
+    response: dict[str, str] = {
+        "message": f"Transaction will be added to Block {index}."
+    }
     return jsonify(response), 201
 
-@app.route(rule='/chain', methods=['GET'])
+
+@app.route(rule="/chain", methods=["GET"])
 def full_chain() -> tuple[Response, Literal[200]]:
     response: dict[str, list[BlockDict] | int] = {
-        'chain': [block.to_dict() for block in blockchain.chain],
-        'length': len(blockchain.chain)
+        "chain": [block.to_dict() for block in blockchain.chain],
+        "length": len(blockchain.chain),
     }
-    
+
     return jsonify(response), 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
